849d0ab445331e63efcf0c6020a392ad4f8c7f92
sabrina mohsenin <smohsenin@gmail.com>
2016-04-10 16:07:03 -0400

    dumplinz

--- dumplinz/js/index.js
+++ dumplinz/js/index.js
@@ -7,4 +7,21 @@ $('.hover').mouseout(function() {
   $('.text').css("visibility","hidden");
 });
 
+$( ".cross" ).hide();
+$( ".responsiveMenu" ).hide();
+
+$( ".hamburger" ).click(function() {
+	$( ".responsiveMenu" ).slideToggle( "slow", function() {
+	$( ".hamburger" ).hide();
+	$( ".cross" ).show();
+	});
+});
+
+$( ".cross" ).click(function() {
+	$( ".responsiveMenu" ).slideToggle( "slow", function() {
+	$( ".cross" ).hide();
+	$( ".hamburger" ).show();
+	});
+});
+
 });
